2f8e38723a931d8784c365622bd501aee566962f
Simon Nawara <sinawara@shi-r4-p4.s19.be>
2024-12-23 12:04:50 +0100

    first commit

---
+++ includes/minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sinawara <sinawara@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/23 11:51:03 by sinawara          #+#    #+#             */
+/*   Updated: 2024/12/23 11:55:52 by sinawara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHEL_H
+
+# include "../libft/include/ft_printf.h"
+# include "../libft/include/get_next_line.h"
+# include "../libft/include/libft.h"
+# include <fcntl.h>
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
