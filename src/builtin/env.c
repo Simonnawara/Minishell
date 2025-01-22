2ed0de89657730aeb677f385eb279c0b8a66a78a
To'Iki Rouillard <trouilla@fu-r7-p6.s19.be>
2025-01-22 12:58:20 +0100

    env ordre decroissant ascii fait

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,31 +6,75 @@
 /*   By: trouilla <trouilla@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 12:19:51 by trouilla          #+#    #+#             */
-/*   Updated: 2025/01/11 12:31:03 by trouilla         ###   ########.fr       */
+/*   Updated: 2025/01/22 12:02:30 by trouilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static int	print_env_error(void)
+static void swap_strings(char **a, char **b)
 {
-	ft_putstr_fd("env: too many arguments\n", 2);
-	return (1);
+    char *temp;
+    
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-int	ft_env(t_exec *exec, char **args)
+static int count_env_vars(char **env)
 {
-	int	i;
+    int count;
 
-	if (!exec || !exec->env)
-		return (1);
-	if (args && args[1])
-		return (print_env_error());
-	i = 0;
-	while (exec->env[i])
-	{
-		ft_putendl_fd(exec->env[i], 1);
-		i++;
-	}
-	return (0);
+    count = 0;
+    while (env && env[count])
+        count++;
+    return (count);
 }
+
+static void sort_env(char **env)
+{
+    int i;
+    int j;
+    int len;
+
+    len = count_env_vars(env);
+    i = 0;
+    while (i < len - 1)
+    {
+        j = 0;
+        while (j < len - i - 1)
+        {
+            if (ft_strcmp(env[j], env[j + 1]) > 0)
+                swap_strings(&env[j], &env[j + 1]);
+            j++;
+        }
+        i++;
+    }
+}
+
+int ft_env(t_exec *exec, char **args)
+{
+    int i;
+
+    if (!exec || !exec->env)
+        return (1);
+    if (args && args[1])
+    {
+        ft_putendl_fd("env: too many arguments", 2);
+        return (1);
+    }
+
+    // Sort the environment directly
+    sort_env(exec->env);
+
+    // Print sorted environment
+    i = 0;
+    while (exec->env[i])
+    {
+        ft_putendl_fd(exec->env[i], 1);
+        i++;
+    }
+
+    return (0);
+}
+
