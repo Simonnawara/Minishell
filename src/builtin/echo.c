363e730c15a034d2ec15a55e9607699c46e430a5
Simon Nawara <sinawara@fu-r7-p7.s19.be>
2025-01-27 14:44:42 +0100

    added 0 handling

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: trouilla <trouilla@student.s19.be>         +#+  +:+       +#+        */
+/*   By: sinawara <sinawara@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 10:51:45 by sinawara          #+#    #+#             */
-/*   Updated: 2025/01/27 13:00:00 by trouilla         ###   ########.fr       */
+/*   Updated: 2025/01/27 14:43:46 by sinawara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -231,8 +231,8 @@ int	ft_echo(char **args, char **res, int echo_counter, t_exec *exec)
 					cleanup_echo_resources(NULL, processed_arg,
 						expanded_arg);
 					free(stripped_arg);
-					free(processed_arg);
-					free(expanded_arg);
+					//free(processed_arg);
+					//free(expanded_arg);
 				}
 			}
 			else
